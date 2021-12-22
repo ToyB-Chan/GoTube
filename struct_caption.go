3fd2b2c56501f62ccd55bc3039d707f46497b036
ToyB-Chan <jasjason0otto@gmail.com>
2021-12-22 04:35:24 +0100

    Made description a bit more clear.

--- struct_caption.go
+++ struct_caption.go
@@ -1,6 +1,6 @@
 package gotube
 
-// Holds information about a single caption. Not used yet.
+// Holds information about a single caption. Not implemented yet.
 type SCaption struct {
 	URL       string `json:"url"` //
 	Extension string `json:"ext"` //
